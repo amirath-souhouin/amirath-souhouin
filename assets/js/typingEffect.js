083b4c891cda0892a17c98d57e4bc8ecb2652ff2
Amirath Souhouin <asouh070@uottawa.ca>
2024-11-15 08:26:09 -0500

    Uodate tyoing effwxt and css transition

--- assets/js/typingEffect.js
+++ assets/js/typingEffect.js
@@ -3,9 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const nameText = "Amirath M.A Souhouin"; // Your full name
   let index = 0;
 
+  if (!nameElement) {
+    console.error("Element with ID 'name' not found.");
+    return;
+  }
+
   function type() {
     if (index < nameText.length) {
-      nameElement.textContent += nameText[index];
+      nameElement.textContent += nameText[index]; // Append one character at a time
       index++;
       setTimeout(type, 100); // Adjust typing speed here
     }
